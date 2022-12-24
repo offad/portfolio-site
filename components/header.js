34f4f15fd0da574396a839b60cbbca4ee55b8823
Oluwanifemi Fadare <79525647+offad@users.noreply.github.com>
2022-12-24 19:38:34 +0000

    Change layouts

--- components/header.js
+++ components/header.js
@@ -10,10 +10,21 @@ class Header extends HTMLElement {
                 <h1>Oluwanifemi Fadare</h2>
 
                 <!-- Add font awesome icons -->
-                <a href="mailto:of.fadare@gmail.com" class="fa fa-envelope"></a>
-                <a href="https://github.com/offad" class="fa fa-github"></a>
-                <a href="https://www.instagram.com/of.fad/" class="fa fa-instagram"></a>
-                <a href="https://www.linkedin.com/in/offad/" class="fa fa-linkedin"></a>  
+                
+                <ul class="links">
+                    <li>
+                        <a href="mailto:of.fadare@gmail.com" class="fa fa-envelope"></a>
+                    </li>
+                    <li>
+                        <a href="https://github.com/offad" class="fa fa-github"></a>
+                    </li>
+                    <li>
+                        <a href="https://www.instagram.com/of.fad/" class="fa fa-instagram"></a>
+                    </li>
+                    <li>
+                        <a href="https://www.linkedin.com/in/offad/" class="fa fa-linkedin"></a>  
+                    </li>
+                </ul>
                 
             </header>   
         `;
